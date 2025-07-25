42390eeb172a2756f46a5091bffb9ca901e13920
John Van Etten <4vanetten@gmail.com>
2025-07-25 16:29:59 -0600

    max steer is not max torque

--- opendbc_repo/opendbc/safety/modes/chrysler.h
+++ opendbc_repo/opendbc/safety/modes/chrysler.h
@@ -132,6 +132,7 @@ static bool chrysler_tx_hook(const CANPacket_t *to_send) {
   };
 
   const TorqueSteeringLimits CHRYSLER_JEEPS_STEERING_LIMITS = {
+    .max_torque = 261,
     .max_rt_delta = 225, // 6 max rate up * 100Hz send rate * 250000 RT interval / 1000000 = 150 ; 150 * 1.5 for safety pad = 225
     .max_rate_up = 6,
     .max_rate_down = 6,
