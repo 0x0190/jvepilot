ffb5341773717b15203ea47c64532c886710216d
John Poon <wasabi@UbuntuVM>
2026-09-02 15:58:40 -0400

    fine tune more

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -21,7 +21,7 @@ from openpilot.common.pid import PIDController
 
 LOW_SPEED_X = [0, 10, 20, 30, 40]
 #LOW_SPEED_Y = [15, 13, 10, 5]
-LOW_SPEED_Y = [15, 13, 10, 2.5, 0.06]   # JSP: Reduce factor at higher speeds to reduce oscillation
+LOW_SPEED_Y = [15, 13, 10, 2.5, 0.03]   # JSP: Reduce factor at higher speeds to reduce oscillation
 
 
 class LatControlTorque(LatControl):
